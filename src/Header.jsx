2569af770fad1f9cee0764b449301654c341f59f
simonojok19 <simonojok19@gmail.com>
2021-08-12 21:03:30 +0300

    using nav link

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+const activeStyle = {
+  color: "purple",
+};
 
 export default function Header() {
   return (
@@ -6,7 +11,19 @@ export default function Header() {
       <nav>
         <ul>
           <li>
-            <img alt="Carved Rock Fitness" src="/images/logo.png" />
+            <NavLink to="/">
+              <img alt="Carved Rock Fitness" src="/images/logo.png" />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/shoes" activeStyle={activeStyle}>
+              Shoes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/cart" activeStyle={activeStyle}>
+              Carts
+            </NavLink>
           </li>
         </ul>
       </nav>
