6fcb7aa14c88200e2015c2c8efd6c25bcdf63fae
simonojok19 <simonojok19@gmail.com>
2021-08-12 20:29:36 +0300

    starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import { getProducts } from "./services/productService";
 import Spinner from "./Spinner";
 import useFetch from "./services/useFetch";
 
