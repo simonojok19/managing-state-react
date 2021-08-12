f01eddf71e845949ab908558cb36f420c42733f7
simonojok19 <simonojok19@gmail.com>
2021-08-12 21:09:36 +0300

    handled the statemanagment for the detail component

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import useFetch from "./services/useFetch";
+import Spinner from "./Spinner";
 
 export default function Detail() {
   const { id } = useParams();
   const { data: product, loading, error } = useFetch(`products/${id}`);
+
+  if (loading) return <Spinner />;
+  if (error) throw error;
 
   return (
     <div id="detail">
