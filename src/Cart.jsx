bd6917381805532d2a0918f871c5a326e849a429
simonojok19 <simonojok19@gmail.com>
2021-08-13 06:53:52 +0300

    implemented the derived state complete

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -41,10 +41,15 @@ export default function Cart({ cart, updateQuantity }) {
 
   if (loading) return <Spinner />;
   if (error) throw error;
+  const numItemsInCart = cart.reduce((total, item) => total + item.quantity, 0);
 
   return (
     <section id="cart">
-      <h1>Cart</h1>
+      <h1>
+        {numItemsInCart === 0
+          ? "Your Cart is Empty"
+          : `${numItemsInCart} Item${numItemsInCart > 1 ? "s" : ""}`}
+      </h1>
       <ul>{cart.map(renderItem)}</ul>
     </section>
   );
