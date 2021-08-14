5f48f56c0168f3c149fcbf916ebf7db33a243b5a
simonojok19 <simonojok19@gmail.com>
2021-08-14 07:36:17 +0300

    added the routing for the cjheckout

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Products from "./Products";
 import { Routes, Route } from "react-router-dom";
 import Detail from "./Detail";
 import Cart from "./Cart";
+import Checkout from "./Checkout";
 
 export default function App() {
   const [cart, setCart] = useState(() => {
@@ -56,6 +57,10 @@ export default function App() {
             <Route
               path="/cart"
               element={<Cart cart={cart} updateQuantity={updateQuantity} />}
+            />
+            <Route
+              path="/checkout"
+              element={<Checkout cart={cart} /?>}
             />
           </Routes>
         </main>
