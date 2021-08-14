d5f9a4ca6fae318d055c57a6dc4f415804845596
simonojok19 <simonojok19@gmail.com>
2021-08-14 09:44:20 +0300

    using ref for statemanagement

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,28 +1,33 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export default function useFetch(url) {
+  const isMountedRef = useRef(false);
   const [data, setData] = useState(null);
   const [error, setError] = useState();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    isMountedRef.current = true;
     async function init() {
       try {
         const response = await fetch(baseUrl + url);
         if (response.ok) {
           const json = await response.json();
-          setData(json);
+          if (isMountedRef.current) setData(json);
         } else {
           throw response;
         }
       } catch (e) {
-        setError(e);
+        if (isMountedRef.current) setError(e);
       } finally {
-        setLoading(false);
+        if (isMountedRef.current) setLoading(false);
       }
     }
     init().then();
+    return () => {
+      isMountedRef.current = false;
+    };
   }, [url]);
   return { data, error, loading };
 }
