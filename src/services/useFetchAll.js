ac0852d0710bf6770ebf31db064576eb4250d50b
simonojok19 <simonojok19@gmail.com>
2021-08-14 09:51:18 +0300

    done with using ref on the elements

--- src/services/useFetchAll.js
+++ src/services/useFetchAll.js
@@ -1,11 +1,25 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+
+const areEqual = (array1, array2) => {
+  return (
+    array1.length === array2.length &&
+    array1.every((value, index) => value === array2[index])
+  );
+};
 
 export default function useFetchAll(urls) {
+  const prevUrlsRef = useRef([]);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    if (areEqual(prevUrlsRef.current, urls)) {
+      setLoading(false);
+      return;
+    }
+    prevUrlsRef.current = urls;
+
     const promises = urls.map((url) =>
       fetch(process.env.REACT_APP_API_BASE_URL + url).then((response) => {
         if (response.ok) return response.json();
@@ -20,8 +34,7 @@ export default function useFetchAll(urls) {
         setError(e);
       })
       .finally(() => setLoading(false));
-    // eslint-disable-next-line
-  }, []);
+  }, [urls]);
 
   return { data, loading, error };
 }
